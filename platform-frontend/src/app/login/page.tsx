4113747737460202439498553cbc2e04cd27a5a9
Edwin M <edwingmundo2@gmail.com>
2024-02-19 18:01:38 -0500

    formating changes

--- platform-frontend/src/app/login/page.tsx
+++ platform-frontend/src/app/login/page.tsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { api } from '~/trpc/server';
-import { cookies } from 'next/headers';
+import React from "react";
+import { api } from "~/trpc/server";
+import { cookies } from "next/headers";
 
 const LoginPage: React.FC = () => {
   async function setCookie(formData: FormData) {
-    'use server'
-    const email = formData.get("email")?.toString()
-    const password = formData.get("password")?.toString()
-    if (email != undefined && password != undefined){
+    "use server";
+    const email = formData.get("email")?.toString();
+    const password = formData.get("password")?.toString();
+    if (email != undefined && password != undefined) {
       const data = await api.auth.login.mutate({
         email: email,
-        password: password
+        password: password,
       });
       cookies().set({
         name: "auth-token",
         value: data,
         httpOnly: true,
-        secure: false
+        secure: false,
       });
     }
   }
   return (
-    <div className="w-full h-screen bg-blue-200 flex flex-col justify-center items-center">
-      <h2 className="mb-10 text-5xl leading-6 font-medium text-gray-900 mb-4">
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-blue-200">
+      <h2 className="mb-10 mb-4 text-5xl font-medium leading-6 text-gray-900">
         Login To Platform
       </h2>
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <form  action={setCookie}>
-          <div className='mt-1'>
+      <div className="rounded-lg bg-white p-8 shadow-md">
+        <form action={setCookie}>
+          <div className="mt-1">
             <label htmlFor="username" className="block text-sm font-medium text-gray-700">
               Email
             </label>
@@ -35,12 +35,25 @@ const LoginPage: React.FC = () => {
               type="text"
               id="email"
               name="email"
-              defaultValue={""} 
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              defaultValue={""}
+              className={`
+                mt-1
+                block
+                w-full
+                rounded-md
+                border
+                border-gray-300
+                px-3
+                py-2
+                shadow-sm
+                focus:border-indigo-500
+                focus:outline-none
+                focus:ring-indigo-500
+                sm:text-sm`}
               required
             />
           </div>
-          <div className='mt-1'>
+          <div className="mt-1">
             <label htmlFor="password" className="block text-sm font-medium text-gray-700">
               Password
             </label>
@@ -49,12 +62,15 @@ const LoginPage: React.FC = () => {
               id="password"
               name="password"
               defaultValue={""}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
               required
             />
           </div>
-          <div className='mt-2'>
-            <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <div className="mt-2">
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
               Submit
             </button>
           </div>
@@ -62,6 +78,6 @@ const LoginPage: React.FC = () => {
       </div>
     </div>
   );
-}
+};
 
 export default LoginPage;
