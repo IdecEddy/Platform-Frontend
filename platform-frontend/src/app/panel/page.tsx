7de115d6f6b81a87849921fe907ccbf354b27ada
Edwin M <edwingmundo2@gmail.com>
2024-02-20 04:47:15 -0500

    Add: middleware for auth
    This is very early in dev and I am sure it is easy to hack right now.

--- platform-frontend/src/app/panel/page.tsx
+++ platform-frontend/src/app/panel/page.tsx
@@ -1,17 +1,6 @@
 'use client'
-import { api } from '~/trpc/react';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 const Panel: React.FC = () => {
-  const router = useRouter();
-  const { data, isLoading: postsLoading } = api.auth.validateToken.useQuery();
-    
-  useEffect(() => {
-    if (data?.auth == false) {
-      router.push("/login");
-    }
-  });
-  return (<p> protected </p>)}
-
+  return (<p> protected </p>)
+}
 export default Panel;
  
---
+++ platform-frontend/src/middleware.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+
+export async function  middleware(request: NextRequest) {
+  // Assuming this is within an async function context
+
+   const token = request.cookies.get('auth-token')?.value;
+  // Redirect immediately if the token doesn't exist
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  const apiUrl = "https://127.0.0.1:8000/api/v1/auth/verify_token";
+  const body = JSON.stringify({
+    token,
+    audience: "platform-frontend",
+  });
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body,
+    });
+
+    // Check for HTTP error responses (e.g., not 2xx)
+    if (!response.ok) {
+      console.error(`Error from auth service: ${response.statusText}`);
+      throw new Error('Authentication failed');
+    }
+
+    // Assuming the API returns { isAuthenticated: boolean }
+    const  isAuthenticated  = await response.json();
+    console.log(isAuthenticated);
+    // If everything is OK, proceed with the request
+    return NextResponse.next();
+  } catch (error: any) {
+    console.error(`Authentication error: ${error.message}`);
+    // Optionally, redirect to a custom error page or log the user out
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+}
+export const config = {
+  matcher: '/panel',
+}
+
