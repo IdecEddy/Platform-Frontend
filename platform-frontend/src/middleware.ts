a7566c29f5907b4b35495cc1dc53801a6b735212
Edwin M <edwingmundo2@gmail.com>
2024-02-21 04:27:53 -0500

    Updates: middleware for auth

--- platform-frontend/src/middleware.ts
+++ platform-frontend/src/middleware.ts
@@ -1,14 +1,13 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-
-export async function  middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   // Assuming this is within an async function context
 
-   const token = request.cookies.get('auth-token')?.value;
+  const token = request.cookies.get("auth-token")?.value;
   // Redirect immediately if the token doesn't exist
   if (!token) {
-    return NextResponse.redirect(new URL('/login', request.url));
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   const apiUrl = "https://127.0.0.1:8000/api/v1/auth/verify_token";
@@ -19,30 +18,28 @@ export async function  middleware(request: NextRequest) {
 
   try {
     const response = await fetch(apiUrl, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body,
     });
 
     // Check for HTTP error responses (e.g., not 2xx)
     if (!response.ok) {
       console.error(`Error from auth service: ${response.statusText}`);
-      throw new Error('Authentication failed');
+      throw new Error("Authentication failed");
     }
 
     // Assuming the API returns { isAuthenticated: boolean }
-    const  isAuthenticated  = await response.json();
+    const isAuthenticated = await response.json();
     console.log(isAuthenticated);
     // If everything is OK, proceed with the request
     return NextResponse.next();
   } catch (error: any) {
     console.error(`Authentication error: ${error.message}`);
     // Optionally, redirect to a custom error page or log the user out
-    return NextResponse.redirect(new URL('/login', request.url));
+    return NextResponse.redirect(new URL("/login", request.url));
   }
-
 }
 export const config = {
-  matcher: '/panel',
-}
-
+  matcher: "/panel",
+};
