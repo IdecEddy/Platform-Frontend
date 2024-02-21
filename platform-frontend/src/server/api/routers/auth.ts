792bd30c4a3d0c6e11c1b54e7cf24138dcdcaa8c
Edwin M <edwingmundo2@gmail.com>
2024-02-21 04:27:31 -0500

    Update: Error handeling of auth code

--- platform-frontend/src/server/api/routers/auth.ts
+++ platform-frontend/src/server/api/routers/auth.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import https from "https";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { cookies } from "next/headers";
@@ -21,31 +21,39 @@ export const authRouter = createTRPCRouter({
       const httpsAgent = new https.Agent({
         rejectUnauthorized: false, // WARNING: This disables SSL certificate validation.
       });
-      const { data } = await axios.post(
-        "https://127.0.0.1:8000/api/v1/auth/login",
-        body,
-        { httpsAgent },
-      );
-      return data.authToken;
-    }),
-  validateToken: publicProcedure
-    .query(async () => {
-      const authToken = cookies().get("auth-token")?.value
-      if (authToken){
-        const body = {
-          token: authToken, 
-          audience: "platform-frontend",
-        };
-        const httpsAgent = new https.Agent({
-          rejectUnauthorized: false, // WARNING: This disables SSL certificate validation.
-        });
-        const { data } = await axios.post(
-          "https://127.0.0.1:8000/api/v1/auth/verify_token",
+      try {
+        const response = await axios.post(
+          "https://127.0.0.1:8000/api/v1/auth/login",
           body,
           { httpsAgent },
         );
-        return { "auth": true, "data": data }
+        if (response.data.authToken) {
+          const authToken = response.data.authToken;
+          return { auth: true, data: authToken, error: undefined };
+        }
+      } catch (error) {
+        if (error.response) {
+          return { auth: false, data: undefined, error: error };
+        }
       }
-      return { "auth": false, "data": {}}
-    })
+    }),
+  validateToken: publicProcedure.query(async () => {
+    const authToken = cookies().get("auth-token")?.value;
+    if (authToken) {
+      const body = {
+        token: authToken,
+        audience: "platform-frontend",
+      };
+      const httpsAgent = new https.Agent({
+        rejectUnauthorized: false, // WARNING: This disables SSL certificate validation.
+      });
+      const { data } = await axios.post(
+        "https://127.0.0.1:8000/api/v1/auth/verify_token",
+        body,
+        { httpsAgent },
+      );
+      return { auth: true, data: data };
+    }
+    return { auth: false, data: {} };
+  }),
 });
