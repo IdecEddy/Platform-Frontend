18464be4f078925ecf668e627839598ba1ac865d
Edwin M <edwingmundo2@gmail.com>
2024-03-01 22:33:01 -0500

    Added: new route configs
    - This is a page that we can use to manage kubectl configs.
    - Right now this is just a copy of the panel page while I work on the backend for this page.

---
+++ platform-frontend/src/app/panel/configs/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import { validateToken } from '~/actions/auth';
+import { DesktopNav } from '~/components/navbar';
+
+const Panel: React.FC = () => {
+  const activeNavItem = "Configs";
+  const [loggedIn, setLogin] = useState(false);
+  const [authToken, setAuthToken] = useState(null);
+  useEffect(() => {
+    async function checkToken() {
+    const ret = await validateToken(authToken);
+      if (ret && ret.status == 200) {
+        setLogin(true);
+        setAuthToken(ret.authToken);
+      } else {
+        setLogin(false);
+      }
+    }
+    checkToken()
+    const interval = setInterval(() => {
+      checkToken()
+    }, 30000);
+    
+    return () =>  clearInterval(interval);
+  }, [authToken]);
+  
+  if (loggedIn == true) {
+    return (
+      <DesktopNav activeItem={activeNavItem} />
+    )
+  }
+};
+export default Panel;
