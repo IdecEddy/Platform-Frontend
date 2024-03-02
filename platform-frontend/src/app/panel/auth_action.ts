612a173ada667b2fd55d0438d817e9c943ae02ae
Edwin M <edwingmundo2@gmail.com>
2024-03-01 22:28:56 -0500

    Remove: Old zombie files
    - We don't use these actions or contexts anymore getting rid of them

--- platform-frontend/src/app/panel/auth_action.ts
+++
@@ -1,7 +0,0 @@
-"use server"
-import { api } from "~/trpc/server"; 
-export async function getAuthToken() {
-  const res = await api.auth.validateToken.query()
-  return res
-
-}
--- platform-frontend/src/app/panel/auth_context.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-
-// Create a context with a default value (optional)
-const AuthContext = React.createContext({ authToken: ""});
-
-export default AuthContext;
