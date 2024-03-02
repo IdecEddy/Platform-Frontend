69403932e613401fc05cfe70b1459c0fc65a1d47
Edwin M <edwingmundo2@gmail.com>
2024-03-01 22:30:28 -0500

    Update: Dynamic Navbar
    - Made it so we have a data struct to store navbar items and link locations.
    - Made it so we use <Link> and not <p> now links work.
    - Some of these are going to nav to a 404 but that is beacuse we dont have thouse routes yet.

--- platform-frontend/src/components/navbar.tsx
+++ platform-frontend/src/components/navbar.tsx
@@ -1,17 +1,28 @@
-export function DesktopNav() {
+import React from 'react';
+import Link from 'next/link';
+
+
+export function DesktopNav({ activeItem } : { activeItem: String }) {
+  const navbarItems = [
+    { name: 'Home', href: '/panel' }, 
+    { name: 'Clusters', href: '/clusters' },
+    { name: 'Nodes', href: '/nodes' },
+    { name: 'Configs', href: '/panel/configs' },
+  ];
   return (
     <div className="w-64 h-screen bg-stone-400 flex flex-col">
       <div className="bg-stone-600 w-full h-16 flex justify-center items-center font-bold text-white">
         Platform
       </div>
       <div className="flex flex-col flex-grow">
-        <p className="p-3 hover:bg-stone-300 transition-colors duration-200 cursor-pointer">Home</p>
-        <p className="p-3 hover:bg-stone-300 transition-colors duration-200 cursor-pointer">Clusters</p>
-        <p className="p-3 hover:bg-stone-300 transition-colors duration-200 cursor-pointer">Nodes</p>
+        {navbarItems.map((item) => (
+          <Link key={item.name} href={item.href} passHref className={`block p-3 hover:bg-stone-300 transition-colors duration-200 cursor-pointer ${activeItem === item.name ? 'bg-stone-500 text-white' : ''}`}>
+              {item.name}
+          </Link>
+        ))}
       </div>
-      <button 
+      <button
         className="w-full py-3 bg-stone-600 text-white font-bold hover:bg-stone-700 transition-colors duration-200"
-        onClick={() => {}}
       >
         Logout
       </button>
